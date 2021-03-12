78e8a25504700bf55c44f203d420166066c06039
piaocg <piaocg@outlook.com>
2021-03-12 21:51:38 +0900

    resuce pin by hash

--- app/controllers/hashes_controller.rb
+++ app/controllers/hashes_controller.rb
@@ -2,12 +2,16 @@ class HashesController < ApplicationController
    def create
       url = "#{Rails.configuration.x.gateway}/#{params[:id]}"
       response = HTTP.get url
-      if response.code == 200
-         uuid = SecureRandom.uuid
-         Temp.create! key: uuid, value: Temp::PINNING
-         PinJob.perform_later params[:id], @user.id, params[:custom_name], uuid
-         succeed uuid
-      else
+      begin
+         if response.code == 200
+            uuid = SecureRandom.uuid
+            Temp.create! key: uuid, value: Temp::PINNING
+            PinJob.perform_later params[:id], @user.id, params[:custom_name], uuid
+            succeed uuid
+         else
+            fail
+         end
+      rescue
          fail
       end
    end
